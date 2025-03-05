56dc3ce7400a2975c8be3b944e4400e74377964e
cmurillos <111917180+cmurillos@users.noreply.github.com>
2025-03-05 14:34:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
-    name="circuit_regressor",
+    name="Circuit_Regressor",
     version="0.1.0",
     author="Carlos Murillo",
     author_email="cmurillos@unal.edu.co",
